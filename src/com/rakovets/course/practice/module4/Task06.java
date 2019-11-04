d1695c45d9ba3bc46c914355068ca1c77077a9ee
Pavel Yanushonak <1237023m@gmail.com>
2019-11-04 20:32:35 +0300

    module 4 tasks 6, 7

--- src/com/rakovets/course/practice/module4/Task06.java
+++ src/com/rakovets/course/practice/module4/Task06.java
@@ -23,6 +23,31 @@ public class Task06 {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-
+		if (amount < 0){
+			System.out.print("-");
+			amount = amount*(-1);
+		}
+		double digits = (long) (Math.ceil(Math.log10(amount)+0.5));
+		long temporaryAmount;
+		long temporaryDigits = (long) digits;
+		while(digits % 3 != 0){
+			temporaryDigits = temporaryDigits - 1;
+			temporaryAmount = (long) (( amount) / (Math.pow(10,temporaryDigits)));
+			System.out.print(temporaryAmount);
+			amount = (long) (amount - temporaryAmount*(Math.pow(10,temporaryDigits)));
+			digits = digits -1;
+			if (digits % 3 == 0){
+				System.out.print(" ");
+			}
+		}
+		for(int i = 1; i <= (digits/3); i++){
+			temporaryDigits = temporaryDigits - 3;
+			temporaryAmount = (long) ((amount) / (Math.pow(10,temporaryDigits)));
+			System.out.print(temporaryAmount);
+			amount = (long) (amount - temporaryAmount*(Math.pow(10,temporaryDigits)));
+			if (temporaryDigits != 0){
+				System.out.print(" ");
+			}
+			}
+		}
 	}
-}
